8ed0b843f6c5d8bb3aebc1e326e06886acf12e3d
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-11-12 18:54:04 +0100

    task 6 fix 11

--- _printf.c
+++ _printf.c
@@ -71,7 +71,10 @@ int _printf(const char *format, ...)
 					string_toprint = va_arg(args, char*);
 					print_with_S(string_toprint, &counter);
 				break;
-
+				case 'p':
+					string_toprint = va_arg(args, void*);
+					print_address(string_toprint, &counter);
+				break;
 				default:
 					write_and_count(&prc, 1, &counter, 1);
 					write_and_count(&format[i], 1, &counter, 1);
