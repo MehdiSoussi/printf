b5f31a3cb603078dc967c5586df71398dd586e03
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-11-14 13:33:04 -0800

    current second betty fix

--- secondary_file.c
+++ secondary_file.c
@@ -114,20 +114,20 @@ void hexqdecimal_numbers_pointers(long int d, int *counter, int capital)
 		if (remainder < 10)
 			hexadecimalnum[j++] = 48 + remainder;
 		else
-			if(capital == 1)
+			if (capital == 1)
 				hexadecimalnum[j++] = 87 + remainder;
 			else
 				hexadecimalnum[j++] = 55 + remainder;
 		quotient = quotient / 16;
 	}
 
-	for(c = j; c <8; c++)
+	for (c = j; c < 8; c++)
 	{
 		write(1, &inc, 1);
 		*counter += 1;
 	}
 
-	for (i = j-1; i >= 0; i--)
+	for (i = j - 1; i >= 0; i--)
 	{
 		write(1, &hexadecimalnum[i], 1);
 		*counter += 1;
