dc9e62f2abcd0fba45ec034cec83e318938ae651
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-11-14 13:44:01 -0800

    current 5th betty fix

--- third_file.c
+++ third_file.c
@@ -9,13 +9,12 @@ int binary_numbers(int d)
 {
 	int res = 0, reminder, pow = 1;
 
-	if(d < 0)
-		return -d;
+	if (d < 0)
+		return (-d);
 
-	do
-	{
+	do {
 		reminder = d % 2;
-		d = d / 2 ;
+		d = d / 2;
 		res = reminder * pow + res;
 		pow = pow * 10;
 	} while (d != 0);
